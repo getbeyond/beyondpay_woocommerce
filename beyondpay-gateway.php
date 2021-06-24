cc4f2079b5f38fb65b086eb1c15e9c2eb651430c
Jakub Jasek <dadasign@gmail.com>
2021-06-24 10:51:50 +0200

    v1.5.1 Changelog / version bump.

--- beyondpay-gateway.php
+++ beyondpay-gateway.php
@@ -5,7 +5,7 @@
  * Author: Beyond
  * Author URI: https://getbeyond.com
  * Plugin URI: https://developer.getbeyond.com
- * Version: 1.5.0
+ * Version: 1.5.1
  * Text Domain: beyond-pay-for-woocommerce
  *
  * Tested up to: 5.7.2
